18d033cc65d0990568de05a1578bc7c63e16e97c
SnejUgal <snejugal@icloud.com>
2018-10-26 20:53:05 +0700

    Add a `get_me` example

---
+++ examples/get_me.rs
@@ -0,0 +1,18 @@
+extern crate tbot;
+
+use tbot::{prelude::*, Bot};
+
+fn main() {
+    let token = std::env::var("BOT_TOKEN").unwrap();
+
+    let bot = Bot::new(&token);
+
+    let request = bot
+        .get_me()
+        .get_request()
+        .unwrap()
+        .map_err(|error| println!("Oops, an error happened: {:#?}", error))
+        .map(|me| println!("Here I am: {:#?}", me));
+
+    tbot::run(request);
+}
