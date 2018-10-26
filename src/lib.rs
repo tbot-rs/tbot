2319af071a529daf37185bbf5d6793c08b4af105
SnejUgal <snejugal@icloud.com>
2018-10-26 20:40:51 +0700

    Add `hyper-tls`

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,22 @@
-extern crate hyper;
-extern crate tokio;
 extern crate futures;
+extern crate hyper;
+extern crate hyper_tls;
 extern crate serde;
 extern crate serde_json;
+extern crate tokio;
 #[macro_use]
 extern crate serde_derive;
 
 #[cfg(feature = "proxy")]
 extern crate hyper_proxy;
 
-mod updates;
 mod bot;
 mod ser_mutlipart;
+mod updates;
+
+pub mod methods;
 pub mod types;
 
-pub use updates::*;
 pub use bot::*;
+pub use tokio::{run, spawn};
+pub use updates::*;
