4215096291931faaed04346eeea4ea7e8ea4fde7
SnejUgal <snejugal@icloud.com>
2018-11-09 19:27:10 +0700

    Make `run` and `spawn` accept arbitrary `Item`

--- src/lib.rs
+++ src/lib.rs
@@ -20,8 +20,35 @@ pub mod methods;
 pub mod types;
 
 pub use self::bot::*;
-pub use self::tokio::{run, spawn};
 pub use self::updates::*;
+
+/// Like re-exported `tokio::run`, but doesn't require `future::Item` to be
+/// `()`.
+///
+/// Most use-caces of `tbot` do not need to use the future's `Item` value,
+/// leading to many `.map(|_| ())` in the code. This code will implicitly map
+/// `Item` to `()`. Note that it does **not** map `Error` to `()`, because
+/// error handling must be done on your own.
+pub fn run<F>(future: F)
+where
+    F: futures::Future<Error = ()> + Send + 'static,
+{
+    tokio::run(future.map(|_| ()));
+}
+
+/// Like re-exported `tokio::spawn`, but doesn't require `future::Item` to be
+/// `()`.
+///
+/// Most use-caces of `tbot` do not need to use the future's `Item` value,
+/// leading to many `.map(|_| ())` in the code. This code will implicitly map
+/// `Item` to `()`. Note that it does **not** map `Error` to `()`, because
+/// error handling must be done on your own.
+pub fn spawn<F>(future: F) -> tokio::executor::Spawn
+where
+    F: futures::Future<Error = ()> + Send + 'static,
+{
+    tokio::spawn(future.map(|_| ()))
+}
 
 pub mod prelude {
     //! Re-exports some traits the compile may demand when working with `tbot`.
