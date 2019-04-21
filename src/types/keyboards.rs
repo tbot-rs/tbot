f3ae1aab16563c7e75e1560cd1a5b41cd40de168
SnejUgal <snejugal@icloud.com>
2019-04-21 22:45:26 +0700

    Run rustfmt

--- src/types/keyboards.rs
+++ src/types/keyboards.rs
@@ -7,6 +7,6 @@ mod reply_keyboard_markup;
 mod reply_keyboard_remove;
 
 pub use {
-    any_keyboard::*, force_reply::*, inline_keyboard::*, reply_keyboard_markup::*,
-    reply_keyboard_remove::*,
+    any_keyboard::*, force_reply::*, inline_keyboard::*,
+    reply_keyboard_markup::*, reply_keyboard_remove::*,
 };
