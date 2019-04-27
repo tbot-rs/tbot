3b1d8a90c45a82528cb22c580d0e76971c1f8a41
SnejUgal <snejugal@icloud.com>
2019-04-28 00:27:58 +0700

    Add `Bot::poll` and `Bot::updated_poll` handlers

--- src/contexts.rs
+++ src/contexts.rs
@@ -3,9 +3,11 @@
 use super::*;
 
 mod edited_text;
+mod poll;
 mod text;
 mod update;
+mod updated_poll;
 
 pub mod traits;
 
-pub use {edited_text::*, text::*, update::*};
+pub use {edited_text::*, poll::*, text::*, update::*, updated_poll::*};
---
+++ src/contexts/updated_poll.rs
@@ -0,0 +1,22 @@
+use super::*;
+use std::sync::Arc;
+
+/// Context for the [`updated_poll`] handler.
+///
+/// [`updated_poll`]: ../struct.Bot.html#method.updated_poll
+#[derive(Clone)]
+pub struct UpdatedPollContext {
+    /// A mock bot with all API methods.
+    pub bot: Arc<MockBot>,
+    /// The poll.
+    pub poll: types::Poll,
+}
+
+impl UpdatedPollContext {
+    pub(crate) fn new(bot: Arc<MockBot>, poll: types::Poll) -> Self {
+        Self {
+            bot,
+            poll,
+        }
+    }
+}
