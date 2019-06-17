33543ec1ee8ca60df8b68cc1a4405b79ebc32a87
SnejUgal <contact@snejugal.ru>
2019-06-17 12:58:55 +0700

    Move `UrlVIsibility` under `parameters`

--- src/types/inline_query/result/article.rs
+++ src/types/inline_query/result/article.rs
@@ -3,30 +3,8 @@
 //! [docs]: ../enum.InlineQueryResult.html#variant.Article
 
 use super::Thumb;
-use crate::types::InputMessageContent;
+use crate::types::{parameters::UrlVisibility, InputMessageContent};
 use serde::Serialize;
-
-/// Represent URL visibility state.
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-// todo: #[non_exhaustive]
-pub enum UrlVisibility {
-    /// The URL is visible.
-    Shown,
-    /// The URL is hidden.
-    Hidden,
-}
-
-impl UrlVisibility {
-    /// Checks if the visibility is set to `Shown`.
-    pub fn is_shown(self) -> bool {
-        self == UrlVisibility::Shown
-    }
-
-    /// Checks if the visibility is set to `Hidden`.
-    pub fn is_hidden(self) -> bool {
-        self == UrlVisibility::Hidden
-    }
-}
 
 /// Represents an [`InlineQueryResultArticle`][docs].
 ///
--- src/types/parameters.rs
+++ src/types/parameters.rs
@@ -4,9 +4,10 @@ mod chat_id;
 mod notification_state;
 mod parse_mode;
 mod updates;
+mod url_visibility;
 mod web_page_preview_state;
 
 pub use {
     chat_id::*, notification_state::*, parse_mode::*, updates::*,
-    web_page_preview_state::*,
+    url_visibility::*, web_page_preview_state::*,
 };
