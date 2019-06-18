3dc8d90ea922cfe74fff6d5e2347d31849282621
SnejUgal <contact@snejugal.ru>
2019-06-18 16:34:46 +0700

    Fix missing `shipping::Option`
    Fuck live share

--- src/types/shipping/option.rs
+++ src/types/shipping/option.rs
@@ -1,1 +1,27 @@
+use crate::types::LabeledPrice;
+use serde::Serialize;
 
+/// Represents a [`ShippingOption`][docs].
+///
+/// [docs]: https://core.telegram.org/bots/api/#shippingoption
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Serialize)]
+pub struct Option<'a> {
+    id: &'a str,
+    title: &'a str,
+    prices: &'a [LabeledPrice<'a>],
+}
+
+impl<'a> Option<'a> {
+    /// Constructs a shipping `Option`.
+    pub fn new(
+        id: &'a str,
+        title: &'a str,
+        prices: &'a [LabeledPrice<'a>],
+    ) -> Self {
+        Self {
+            id,
+            title,
+            prices,
+        }
+    }
+}
