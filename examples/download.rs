5a3b034acbb042eb308a52bc0d4f564ed826d729
SnejUgal <contact@snejugal.ru>
2019-06-21 20:34:19 +0700

    Make an example how to download files

---
+++ examples/download.rs
@@ -0,0 +1,32 @@
+use std::sync::Arc;
+use tbot::prelude::*;
+
+fn main() {
+    let mut bot = tbot::bot!("BOT_TOKEN").event_loop();
+
+    bot.document(|context| {
+        let bot = Arc::clone(&context.bot);
+        let document = context
+            .bot
+            .get_file(&context.document)
+            .into_future()
+            .map_err(|err| {
+                dbg!(err);
+            })
+            .and_then(move |file| {
+                bot.download_file(&file).map_err(|err| {
+                    dbg!(err);
+                })
+            })
+            .map(|bytes| match String::from_utf8(bytes) {
+                Ok(document) => println!("{}", document),
+                Err(err) => {
+                    dbg!(err);
+                }
+            });
+
+        tbot::spawn(document);
+    });
+
+    bot.polling().start();
+}
