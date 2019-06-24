b3b9631a70dac0dd04c80184a876950bbd06e326
SnejUgal <contact@snejugal.ru>
2019-06-24 19:07:54 +0700

    Rewrite the location example with commands

--- examples/location.rs
+++ examples/location.rs
@@ -1,8 +1,7 @@
 use std::time::{Duration, Instant};
-use tbot::{prelude::*, types::chat};
+use tbot::prelude::*;
 use tokio::{prelude::*, timer::Interval};
 
-const CHAT: chat::Id = chat::Id(0);
 const INTERVAL: u64 = 15;
 const PLACES: [(f64, f64); 6] = [
     (38.904722, -77.016389), // Washington
@@ -15,38 +14,41 @@ const PLACES: [(f64, f64); 6] = [
 const UPDATE_PERIOD: u32 = 3600 * 24;
 
 fn main() {
-    let bot = tbot::bot!("BOT_TOKEN");
-    let mut places = PLACES.iter().cycle();
+    let mut bot = tbot::bot!("BOT_TOKEN").event_loop();
 
-    let live_location = bot
-        .send_location(CHAT, *places.next().unwrap())
-        .live_period(UPDATE_PERIOD)
-        .into_future()
-        .map_err(|err| {
-            dbg!(err);
-        })
-        .and_then(|message| {
-            Interval::new(Instant::now(), Duration::from_secs(INTERVAL))
-                .skip(1)
-                .zip(futures::stream::iter_ok(places))
-                .map_err(|err| {
-                    dbg!(err);
-                })
-                .for_each(move |(_, place)| {
-                    bot.edit_message_location(
-                        message.chat.id,
-                        message.id,
-                        *place,
-                    )
-                    .into_future()
+    bot.command("location", |context| {
+        let context = context.clone();
+        let mut places = PLACES.iter().cycle();
+
+        let location = context
+            .send_location(*places.next().unwrap())
+            .live_period(UPDATE_PERIOD)
+            .into_future()
+            .map_err(|err| {
+                dbg!(err);
+            })
+            .and_then(|message| {
+                Interval::new(Instant::now(), Duration::from_secs(INTERVAL))
+                    .skip(1)
+                    .zip(futures::stream::iter_ok(places))
                     .map_err(|err| {
                         dbg!(err);
                     })
-                    .map(|_| ())
-                })
-        })
-        .timeout(Duration::from_secs(UPDATE_PERIOD as u64))
-        .map_err(|_| ()); // expected to timeout
+                    .for_each(move |(_, place)| {
+                        context
+                            .edit_message_location(message.id, *place)
+                            .into_future()
+                            .map_err(|err| {
+                                dbg!(err);
+                            })
+                            .map(|_| ())
+                    })
+            })
+            .timeout(Duration::from_secs(UPDATE_PERIOD as u64))
+            .map_err(|_| ()); // expected to timeout
 
-    tbot::run(live_location);
+        tbot::spawn(location);
+    });
+
+    bot.polling().start();
 }
