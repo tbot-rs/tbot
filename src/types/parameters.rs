e861d0f5c7d1d546233d9e09acc6125afb97aa91
AlexStrNik <alex.str.nik@gmail.com>
2019-06-25 18:14:38 +0300

    Remove `ParseMode` from public API
    Co-authored-by: SnejUgal <contact@snejugal.ru>

--- src/types/parameters.rs
+++ src/types/parameters.rs
@@ -12,8 +12,9 @@ mod updates;
 mod url_visibility;
 mod web_page_preview_state;
 
+pub(crate) use parse_mode::*;
 pub use {
-    chat_id::*, flexibility::*, notification_state::*, parse_mode::*, photo::*,
+    chat_id::*, flexibility::*, notification_state::*, photo::*,
     requirement::*, send_to_provider_state::*, text::*, updates::*,
     url_visibility::*, web_page_preview_state::*,
 };
