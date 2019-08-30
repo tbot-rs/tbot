fa57022ce25e4b522a936430c179b1f8e776d2d1
SnejUgal <contact@snejugal.ru>
2019-08-30 19:10:24 +0700

    Move traits related to messages in one module

--- src/contexts/fields/edited_message.rs
+++
@@ -1,7 +0,0 @@
-use super::MediaMessage;
-
-/// A general trait for edited messages.
-pub trait EditedMessage<C>: MediaMessage<C> {
-    /// The last time when the message was edited.
-    fn edit_date(&self) -> i64;
-}
