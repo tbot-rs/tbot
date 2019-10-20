df8c16a4a1e461486048ad39d588c528f7a5cb93
SnejUgal <contact@snejugal.ru>
2019-10-20 19:08:17 +0700

    Move polling to async/.await

--- src/errors.rs
+++ src/errors.rs
@@ -4,5 +4,10 @@ mod download;
 mod http_webhook;
 mod https_webhook;
 mod method_call;
+mod polling;
+mod polling_setup;
 
-pub use {download::*, http_webhook::*, https_webhook::*, method_call::*};
+pub use {
+    download::*, http_webhook::*, https_webhook::*, method_call::*, polling::*,
+    polling_setup::*,
+};
