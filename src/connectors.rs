b0c9a4748c3836716ba6254518b0a076ae2b7323
SnejUgal <contact@snejugal.ru>
2019-10-20 18:52:18 +0700

    Remove the `proxy` feature for now
    tbot won't compile with the `proxy` feature right now because
    hyper-proxy isn't compatible with hyper v0.13 yet. This commit must be
    reverted as soon as hyper-proxy supports hyper v0.13.

--- src/connectors.rs
+++ src/connectors.rs
@@ -1,8 +1,5 @@
 //! A few common connectors for making requests.
 
-#[cfg(feature = "proxy")]
-pub use hyper_proxy as proxy;
-
 use crate::internal;
 use hyper::{
     client::{connect::Connect, HttpConnector},
@@ -10,15 +7,10 @@ use hyper::{
 };
 use hyper_tls::HttpsConnector;
 
-#[cfg(feature = "proxy")]
-use proxy::ProxyConnector;
 
 /// The default HTTPS connector.
 pub type Https = HttpsConnector<HttpConnector>;
 
-#[cfg(feature = "proxy")]
-/// The default proxy connector.
-pub type Proxy = ProxyConnector<Https>;
 
 /// Constructs a HTTPS connector.
 pub fn https() -> Https {
@@ -30,13 +22,6 @@ pub fn https() -> Https {
     })
 }
 
-#[cfg(feature = "proxy")]
-/// Constructs a proxy connector.
-pub fn proxy(proxy: proxy::Proxy) -> Proxy {
-    ProxyConnector::from_proxy(https(), proxy).unwrap_or_else(|error| {
-        panic!("[tbot] Failed to construct a proxy connector: {:#?}", error)
-    })
-}
 
 pub(crate) fn create_client<C: Connector>(connector: C) -> internal::Client<C> {
     Client::builder()
