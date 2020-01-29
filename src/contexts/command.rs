c5dcda1122528937aa93233f9e5b0085dcd20062
AlexStrNik <alex.str.nik@gmail.com>
2020-01-29 18:11:47 +0300

    Implement context traits and fields for the `Command` context

--- src/contexts/command.rs
+++ src/contexts/command.rs
@@ -1,3 +1,20 @@
+use crate::{
+    contexts::{
+        fields::{
+            self, AnyText, Context, EditedMessage, Forward, MediaMessage,
+            Message,
+        },
+        traits::{Forwardable, Pinnable},
+        EditedText, Text,
+    },
+    internal::Sealed,
+    types::{
+        self,
+        message::{self, inline_markup},
+        Chat, User,
+    },
+    Bot,
+};
 use std::ops::Deref;
 
 /// A wrapping context for commands.
@@ -21,3 +38,149 @@ impl<C> Command<C> {
         Self { command, context }
     }
 }
+
+// Prepare for hell.
+
+impl<C> Sealed for Command<Text<C>> {}
+
+impl<C> Context<C> for Command<Text<C>> {
+    fn bot(&self) -> &Bot<C> {
+        self.context.bot()
+    }
+}
+
+impl<C> Message<C> for Command<Text<C>> {
+    #[must_use]
+    fn message_id(&self) -> message::Id {
+        self.context.message_id()
+    }
+
+    #[must_use]
+    fn from(&self) -> Option<&User> {
+        self.context.from()
+    }
+
+    #[must_use]
+    fn date(&self) -> i64 {
+        self.context.date()
+    }
+
+    #[must_use]
+    fn chat(&self) -> &Chat {
+        self.context.chat()
+    }
+}
+
+impl<C> MediaMessage<C> for Command<Text<C>> {
+    #[must_use]
+    fn reply_to(&self) -> Option<&types::Message> {
+        self.context.reply_to()
+    }
+
+    #[must_use]
+    fn author_signature(&self) -> Option<&str> {
+        self.context.author_signature()
+    }
+
+    #[must_use]
+    fn reply_markup(&self) -> Option<&inline_markup::Keyboard> {
+        self.context.reply_markup()
+    }
+}
+
+impl<C> Forward<C> for Command<Text<C>> {
+    #[must_use]
+    fn forward(&self) -> Option<&message::Forward> {
+        self.context.forward()
+    }
+}
+
+impl<C> fields::Text<C> for Command<Text<C>> {
+    #[must_use]
+    fn text(&self) -> &message::Text {
+        &self.context.text
+    }
+}
+
+impl<C> AnyText<C> for Command<Text<C>> {
+    #[must_use]
+    fn text(&self) -> &message::Text {
+        &self.context.text
+    }
+}
+
+impl<'a, C: 'static> Forwardable<'a, C> for Command<Text<C>> {}
+impl<'a, C: 'static> Pinnable<'a, C> for Command<Text<C>> {}
+
+// Once again.
+
+impl<C> Sealed for Command<EditedText<C>> {}
+
+impl<C> Context<C> for Command<EditedText<C>> {
+    fn bot(&self) -> &Bot<C> {
+        self.context.bot()
+    }
+}
+
+impl<C> Message<C> for Command<EditedText<C>> {
+    #[must_use]
+    fn message_id(&self) -> message::Id {
+        self.context.message_id()
+    }
+
+    #[must_use]
+    fn from(&self) -> Option<&User> {
+        self.context.from()
+    }
+
+    #[must_use]
+    fn date(&self) -> i64 {
+        self.context.date()
+    }
+
+    #[must_use]
+    fn chat(&self) -> &Chat {
+        self.context.chat()
+    }
+}
+
+impl<C> MediaMessage<C> for Command<EditedText<C>> {
+    #[must_use]
+    fn reply_to(&self) -> Option<&types::Message> {
+        self.context.reply_to()
+    }
+
+    #[must_use]
+    fn author_signature(&self) -> Option<&str> {
+        self.context.author_signature()
+    }
+
+    #[must_use]
+    fn reply_markup(&self) -> Option<&inline_markup::Keyboard> {
+        self.context.reply_markup()
+    }
+}
+
+impl<C> EditedMessage<C> for Command<EditedText<C>> {
+    #[must_use]
+    fn edit_date(&self) -> i64 {
+        self.context.edit_date()
+    }
+}
+
+impl<C> fields::Text<C> for Command<EditedText<C>> {
+    #[must_use]
+    fn text(&self) -> &message::Text {
+        &self.context.text
+    }
+}
+
+impl<C> AnyText<C> for Command<EditedText<C>> {
+    #[must_use]
+    fn text(&self) -> &message::Text {
+        &self.context.text
+    }
+}
+
+impl<'a, C: 'static> Forwardable<'a, C> for Command<EditedText<C>> {}
+impl<'a, C: 'static> Pinnable<'a, C> for Command<EditedText<C>> {}
