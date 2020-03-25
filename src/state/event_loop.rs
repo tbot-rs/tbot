a58ae5e3eee0f72f8971ca67856f1cb311c4e47d
SnejUgal <contact@snejugal.ru>
2020-03-25 16:11:00 +0700

    Implement `get_state` and all non-edited command handlers

--- src/state/event_loop.rs
+++ src/state/event_loop.rs
@@ -17,6 +17,11 @@ impl<C, S> StatefulEventLoop<C, S> {
         }
     }
 
+    /// Returns an `Arc` to the state.
+    pub fn get_state(&self) -> Arc<S> {
+        Arc::clone(&self.state)
+    }
+
     /// Sets the bot's username.
     ///
     /// The username is used when checking if a command such as
@@ -56,6 +61,22 @@ impl<C, S: Send + Sync + 'static> StatefulEventLoop<C, S> {
         });
     }
 
+    /// Adds a new handler for a sequence of commands.
+    pub fn commands<Cm, H, F>(&mut self, commands: Cm, handler: H)
+    where
+        Cm: IntoIterator<Item = &'static str>,
+        H: (Fn(Arc<contexts::Command<contexts::Text<C>>>, Arc<S>) -> F)
+            + Send
+            + Sync
+            + 'static,
+        F: Future<Output = ()> + Send + 'static,
+    {
+        let state = Arc::clone(&self.state);
+        self.inner.commands(commands, move |context| {
+            handler(context, Arc::clone(&state))
+        });
+    }
+
     /// Adds a new handler for the `/start` command.
     pub fn start<H, F>(&mut self, handler: H)
     where
@@ -66,6 +87,30 @@ impl<C, S: Send + Sync + 'static> StatefulEventLoop<C, S> {
         F: Future<Output = ()> + Send + 'static,
     {
         self.command("start", handler);
+    }
+
+    /// Adds a new handler for the `/help` command.
+    pub fn help<H, F>(&mut self, handler: H)
+    where
+        H: (Fn(Arc<contexts::Command<contexts::Text<C>>>, Arc<S>) -> F)
+            + Send
+            + Sync
+            + 'static,
+        F: Future<Output = ()> + Send + 'static,
+    {
+        self.command("help", handler);
+    }
+
+    /// Adds a new handler for the `/settings` command.
+    pub fn settings<H, F>(&mut self, handler: H)
+    where
+        H: (Fn(Arc<contexts::Command<contexts::Text<C>>>, Arc<S>) -> F)
+            + Send
+            + Sync
+            + 'static,
+        F: Future<Output = ()> + Send + 'static,
+    {
+        self.command("settings", handler);
     }
 
     /// Adds a new handler for text messages.
