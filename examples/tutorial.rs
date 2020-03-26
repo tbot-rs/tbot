09f50dbaa6c95a156e80bfe700b7c462ccdf9898
AlexStrNik <alex.str.nik@gmail.com>
2020-03-26 12:40:35 +0300

    Update tutorial

--- examples/tutorial.rs
+++ examples/tutorial.rs
@@ -1,4 +1,3 @@
-use std::sync::Arc;
 use tbot::{
     markup::{inline_code, markdown_v2},
     prelude::*,
@@ -13,9 +12,10 @@ use tokio::sync::Mutex;
 
 #[tokio::main]
 async fn main() {
-    let mut bot = Bot::from_env("BOT_TOKEN").event_loop();
+    let mut bot =
+        Bot::from_env("BOT_TOKEN").stateful_event_loop(Mutex::new(0_u32));
 
-    bot.text(|context| async move {
+    bot.text(|context, _| async move {
         let calc_result = meval::eval_str(&context.text.value);
         let message = if let Ok(answer) = calc_result {
             markdown_v2(("= ", inline_code([answer.to_string()]))).to_string()
@@ -31,41 +31,36 @@ async fn main() {
         }
     });
 
-    let id = Arc::new(Mutex::new(0_u32));
-    bot.inline(move |context| {
-        let id = Arc::clone(&id);
-        async move {
-            let calc_result = meval::eval_str(&context.query);
-            let (title, message) = if let Ok(answer) = calc_result {
-                let answer = answer.to_string();
-                let message = markdown_v2(inline_code([
-                    context.query.as_str(),
-                    " = ",
-                    answer.as_str(),
-                ]))
+    bot.inline(move |context, id| async move {
+        let calc_result = meval::eval_str(&context.query);
+        let (title, message) = if let Ok(answer) = calc_result {
+            let answer = answer.to_string();
+            let message = markdown_v2(inline_code([
+                context.query.as_str(),
+                " = ",
+                answer.as_str(),
+            ]))
+            .to_string();
+            (answer, message)
+        } else {
+            let title = "Whops...".into();
+            let message = markdown_v2("I couldn't evaluate your expression :(")
                 .to_string();
-                (answer, message)
-            } else {
-                let title = "Whops...".into();
-                let message =
-                    markdown_v2("I couldn't evaluate your expression :(")
-                        .to_string();
-                (title, message)
-            };
+            (title, message)
+        };
 
-            let id = {
-                let mut id = id.lock().await;
-                *id += 1;
-                id.to_string()
-            };
-            let content = Text::new(ParseMode::markdown_v2(&message));
-            let article = Article::new(&title, content).description(&message);
-            let result = inline_query::Result::new(&id, article);
+        let id = {
+            let mut id = id.lock().await;
+            *id += 1;
+            id.to_string()
+        };
+        let content = Text::new(ParseMode::markdown_v2(&message));
+        let article = Article::new(&title, content).description(&message);
+        let result = inline_query::Result::new(&id, article);
 
-            let call_result = context.answer(&[result]).call().await;
-            if let Err(err) = call_result {
-                dbg!(err);
-            }
+        let call_result = context.answer(&[result]).call().await;
+        if let Err(err) = call_result {
+            dbg!(err);
         }
     });
 
