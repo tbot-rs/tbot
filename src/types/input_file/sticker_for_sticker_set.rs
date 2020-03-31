4c383fed5bb9718fbce2964a7ab937ccb1450405
SnejUgal <contact@snejugal.ru>
2020-03-31 17:27:17 +0700

    Support uploading animated stickers
    This change won't cause incompatibilities as `PngSticker`s are still
    accepted by `impl Into<StickerForStickerSet<'_>>`.

---
+++ src/types/input_file/sticker_for_sticker_set.rs
@@ -0,0 +1,22 @@
+use super::{PngSticker, TgsSticker};
+
+/// Represents a sticker that can be added to a sticker set.
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub enum StickerForStickerSet<'a> {
+    /// A PNG sticker.
+    Png(PngSticker<'a>),
+    /// A TGS (animated) sticker.
+    Tgs(TgsSticker<'a>),
+}
+
+impl<'a> From<PngSticker<'a>> for StickerForStickerSet<'a> {
+    fn from(sticker: PngSticker<'a>) -> Self {
+        Self::Png(sticker)
+    }
+}
+
+impl<'a> From<TgsSticker<'a>> for StickerForStickerSet<'a> {
+    fn from(sticker: TgsSticker<'a>) -> Self {
+        Self::Tgs(sticker)
+    }
+}
