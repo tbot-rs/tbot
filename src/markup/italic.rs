97bf86d34747dd17f2a67a46b90ee1735d2c76f9
SnejUgal <contact@snejugal.ru>
2020-04-28 15:32:21 +0700

    Fix sibling italics not stringified correctly

--- src/markup/italic.rs
+++ src/markup/italic.rs
@@ -1,5 +1,5 @@
 use super::{html, markdown_v2, Formattable, Nesting};
-use std::fmt::{self, Formatter, Write};
+use std::fmt::{self, Formatter};
 
 /// Formats text in italic. Can be created with [`italic`].
 ///
@@ -19,7 +19,7 @@ impl<T: Formattable> markdown_v2::Formattable for Italic<T> {
         nesting: Nesting,
     ) -> fmt::Result {
         if !nesting.italic {
-            formatter.write_char('_')?;
+            formatter.write_str("\r_")?;
         }
         markdown_v2::Formattable::format(
             &self.0,
@@ -30,7 +30,7 @@ impl<T: Formattable> markdown_v2::Formattable for Italic<T> {
             },
         )?;
         if !nesting.italic {
-            formatter.write_char('_')?;
+            formatter.write_str("\r_")?;
         }
         Ok(())
     }
@@ -45,5 +45,17 @@ impl<T: Formattable> html::Formattable for Italic<T> {
         formatter.write_str("<i>")?;
         html::Formattable::format(&self.0, formatter, nesting)?;
         formatter.write_str("</i>")
+    }
+}
+
+#[cfg(tests)]
+mod tests {
+    use super::{italic, markdown_v2};
+    #[test]
+    fn sibling_italics_are_displayed_correctly() {
+        assert_eq!(
+            markdown_v2((italic("a"), italic("b"))).to_string(),
+            "\r_a\r_\r_b\r_"
+        );
     }
 }
