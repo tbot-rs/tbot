8f5ff56c01592fbf16537eee22b56b5fa66fddc8
AlexStrNik <alex.str.nik@gmail.com>
2020-04-28 11:28:11 +0300

    Refactor `handlers_macros`

--- src/event_loop/handlers_macros.rs
+++ src/event_loop/handlers_macros.rs
@@ -1,30 +1,34 @@
 macro_rules! handler {
     (
         #[doc = $doc:literal]
-        $handlers:ident,
         $name:ident,
         $context:path,
-        $run_handlers:ident,
-        $($will_handle:ident,)?
     ) => {
-        #[doc = $doc]
-        pub fn $name<H, F>(&mut self, handler: H)
-        where
-            H: (Fn(std::sync::Arc<$context>) -> F) + Send + Sync + 'static,
-            F: std::future::Future<Output = ()> + Send + 'static,
-        {
-            self.$handlers.push(Box::new(move |context| {
-                tokio::spawn(handler(context));
-            }))
+        paste::item! {
+            #[doc = $doc]
+            pub fn $name<H, F>(&mut self, handler: H)
+            where
+                H: (Fn(std::sync::Arc<$context>) -> F) + Send + Sync + 'static,
+                F: std::future::Future<Output = ()> + Send + 'static,
+            {
+                self.[<$name _handlers>].push(Box::new(move |context| {
+                    tokio::spawn(handler(context));
+                }))
+            }
         }
 
-        $(fn $will_handle(&self) -> bool {
-            !self.$handlers.is_empty()
-        })?
+        paste::item! {
+            #[allow(dead_code)]
+            fn [<will_handle_ $name>](&self) -> bool {
+                !self.[<$name _handlers>].is_empty()
+            }
+        }
 
-        fn $run_handlers(&self, context: std::sync::Arc<$context>) {
-            for handler in &self.$handlers {
-                handler(context.clone());
+        paste::item! {
+            fn [<run_ $name _handlers>](&self, context: std::sync::Arc<$context>) {
+                &self.[<$name _handlers>].iter().for_each(|handler| {
+                    handler(context.clone());
+                });
             }
         }
     };
