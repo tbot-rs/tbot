d1c3068ca1f977f821c49c317267e44de0508194
AlexStrNik <alex.str.nik@gmail.com>
2020-04-29 18:34:17 +0300

    Implement `without_state`

--- src/predicates.rs
+++ src/predicates.rs
@@ -5,6 +5,25 @@ pub mod media;
 pub mod message;
 mod traits;
 
+use futures::future::BoxFuture;
+use std::sync::Arc;
 pub use traits::{
     PredicateBooleanOperations, StatefulPredicateBooleanOperations,
 };
+
+/// Allows running stateless predicates in the stateful event loop.
+pub fn without_state<'a, C, P, S>(
+    predicate: P,
+) -> impl Fn(Arc<C>, Arc<S>) -> BoxFuture<'a, bool> + Send + Sync + 'a
+where
+    P: Fn(Arc<C>) -> BoxFuture<'a, bool> + Send + Sync + 'a,
+    C: Send + Sync + 'static,
+    S: Send + Sync + 'static,
+{
+    let predicate = Arc::new(predicate);
+
+    move |ctx, _state| {
+        let predicate = Arc::clone(&predicate);
+        Box::pin(async move { predicate(ctx).await })
+    }
+}
