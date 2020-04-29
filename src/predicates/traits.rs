2f1c259971168e7aa597b63ef5a1ee555c9e4a19
AlexStrNik <alex.str.nik@gmail.com>
2020-04-29 15:50:49 +0000

    Apply suggestion to src/predicates/traits.rs

--- src/predicates/traits.rs
+++ src/predicates/traits.rs
@@ -79,7 +79,7 @@ where
     }
 }
 
-type BoxedPredicate<'a, C, S> =
+type BoxedStatefulPredicate<'a, C, S> =
     Box<dyn Fn(Arc<C>, Arc<S>) -> BoxFuture<'a, bool> + Send + Sync + 'a>;
 
 /// Boolean operations on async functions results.
