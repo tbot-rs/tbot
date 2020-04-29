76750ec797851f7260ff0a293bc575a55e582187
AlexStrNik <alex.str.nik@gmail.com>
2020-04-29 19:02:01 +0300

    Improve example

--- examples/predicates.rs
+++ examples/predicates.rs
@@ -18,18 +18,6 @@ async fn is_message_short(context: Arc<Text>) -> bool {
 async fn main() {
     let mut bot = Bot::from_env("BOT_TOKEN").event_loop();
 
-    bot.text_if(is_private, |context| async move {
-        // We are sure that `from` is Some
-        context
-            .send_message_in_reply(&format!(
-                "Hello {name}!",
-                name = &context.from.as_ref().unwrap().first_name
-            ))
-            .call()
-            .await
-            .unwrap();
-    });
-
     bot.text_if(
         is_supergroup.or(is_group).and(is_message_short.not()),
         |context| async move {
@@ -43,7 +31,7 @@ async fn main() {
 
     bot.text_if(is_message_short, |context| async move {
         context
-            .send_message_in_reply("Message is too short!")
+            .send_message_in_reply("The message is too short!")
             .call()
             .await
             .unwrap();
