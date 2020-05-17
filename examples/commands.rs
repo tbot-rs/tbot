762f7d3dcb80affa2ed19f0f2025dddf6ca6d85c
AlexStrNik <alex.str.nik@gmail.com>
2020-05-17 14:34:53 +0300

    Format example

--- examples/commands.rs
+++ examples/commands.rs
@@ -1,19 +1,20 @@
-use tbot::{
-    prelude::*,
-    Bot,
-};
+use tbot::{prelude::*, Bot};
 
 #[tokio::main]
 async fn main() {
     let mut bot = Bot::from_env("BOT_TOKEN").event_loop();
 
-    bot.command_with_description("hello", "Sends hello", |context| async move {
-        context
-            .send_message_in_reply("Hello!")
-            .call()
-            .await
-            .unwrap();
-    });
+    bot.command_with_description(
+        "hello",
+        "Sends hello",
+        |context| async move {
+            context
+                .send_message_in_reply("Hello!")
+                .call()
+                .await
+                .unwrap();
+        },
+    );
 
     bot.help_with_description("Shows help", |context| async move {
         context
