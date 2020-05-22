85625afe5b9da0dcb9cf67e46a8a26b692b416ae
crash <crash@inventati.org>
2020-05-22 20:05:53 +0200

    Replace &'a str fields with Cow<'a, str> in types::login_url

--- src/types/login_url.rs
+++ src/types/login_url.rs
@@ -1,25 +1,26 @@
 use serde::Serialize;
+use std::borrow::Cow;
 
 /// Represents a [`LoginUrl`].
 ///
 /// [`LoginUrl`]: https://core.telegram.org/bots/api#loginurl
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Serialize)]
+#[derive(Debug, PartialEq, Eq, Clone, Hash, Serialize)]
 #[must_use]
 pub struct LoginUrl<'a> {
-    url: &'a str,
+    url: Cow<'a, str>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    forward_text: Option<&'a str>,
+    forward_text: Option<Cow<'a, str>>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    bot_username: Option<&'a str>,
+    bot_username: Option<Cow<'a, str>>,
     #[serde(skip_serializing_if = "Option::is_none")]
     request_write_access: Option<bool>,
 }
 
 impl<'a> LoginUrl<'a> {
     /// Constructs a new `LoginUrl`.
-    pub const fn new(url: &'a str) -> Self {
+    pub fn new(url: impl Into<Cow<'a, str>>) -> Self {
         Self {
-            url,
+            url: url.into(),
             forward_text: None,
             bot_username: None,
             request_write_access: None,
@@ -27,14 +28,14 @@ impl<'a> LoginUrl<'a> {
     }
 
     /// Configures `forward_text`.
-    pub fn forward_text(mut self, text: &'a str) -> Self {
-        self.forward_text = Some(text);
+    pub fn forward_text(mut self, text: impl Into<Cow<'a, str>>) -> Self {
+        self.forward_text = Some(text.into());
         self
     }
 
     /// Configures `bot_username`.
-    pub fn bot_username(mut self, username: &'a str) -> Self {
-        self.bot_username = Some(username);
+    pub fn bot_username(mut self, username: impl Into<Cow<'a, str>>) -> Self {
+        self.bot_username = Some(username.into());
         self
     }
 
