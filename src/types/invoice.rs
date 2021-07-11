7cfa91bcbc056dba0e5f1113c2edfb9dba5828cc
SnejUgal <contact@snejugal.ru>
2021-07-11 19:01:42 +0700

    Deserialize empty `start_parameter` as `None`

--- src/types/invoice.rs
+++ src/types/invoice.rs
@@ -1,4 +1,4 @@
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
 
 /// Represents an [`Invoice`].
 ///
@@ -11,9 +11,25 @@ pub struct Invoice {
     /// The description of the invoice.
     pub description: String,
     /// The start parameter of the invoice.
+    #[serde(deserialize_with = "deserialize_start_parameter")]
     pub start_parameter: Option<String>,
     /// The currency of the invoice.
     pub currency: String,
     /// The total amount of the invoice.
     pub total_amount: u32,
+}
+
+fn deserialize_start_parameter<'de, D>(
+    deserializer: D,
+) -> Result<Option<String>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let string = String::deserialize(deserializer)?;
+
+    Ok(if string.is_empty() {
+        None
+    } else {
+        Some(string)
+    })
 }
